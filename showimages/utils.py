9502dc1959f015196b4df050cf232015212c4c9b
2022Jean <6159984@gmail.com>
2023-05-15 22:27:47 +0800

    feat: 用户的每次请求都会更新session有效期

--- showimages/utils.py
+++ showimages/utils.py
@@ -3,11 +3,14 @@
     :copyright: © 2023 高天驰 <6159984@gmail.com>
 """
 
+import datetime
 import hashlib
+import json
 import os
 import time
 
-from flask import Request
+from flask import Request, Flask
+from flask.sessions import SessionMixin
 
 
 def generate_user_id(request: Request):
@@ -19,3 +22,33 @@ def generate_user_id(request: Request):
     user_id = hashlib.sha256((user_agent+screen_height+screen_width+t).encode('utf-8')).hexdigest()
     
     return user_id
+
+
+def update_user_session(app: Flask, session: SessionMixin, request: Request):
+    """用户的每次请求都会更新session有效期"""
+
+    class DatetimeEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, datetime.datetime):
+                return obj.isoformat()
+            return json.JSONEncoder.default(self, obj)
+        
+    now = datetime.datetime.now()
+    user_id = session.get("USER_ID", "")
+    session['EXPIRATION_TIME'] = now + app.config["PERMANENT_SESSION_LIFETIME"]
+    
+    if not user_id:
+        user_id = generate_user_id(request)
+        session['USER_ID'] = user_id
+
+    try:
+        with open(app.config["USER_EXPIRATION_TIME_LOG"], "r") as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    data = data.copy()
+    data[user_id] = session['EXPIRATION_TIME']
+
+    with open(app.config["USER_EXPIRATION_TIME_LOG"], "w") as f:
+        json.dump(data, f, indent=4, cls=DatetimeEncoder)
