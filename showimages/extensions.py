e0cb51174ca472f7b3087d00772d5ce9a319d481
2022Jean <6159984@gmail.com>
2023-05-20 21:43:24 +0800

    feat: 添加令牌拓展

--- showimages/extensions.py
+++ showimages/extensions.py
@@ -3,12 +3,6 @@
     :copyright: © 2023 高天驰 <6159984@gmail.com>
 """
 
-from flask import Flask
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf.csrf import CSRFProtect
 
-
-photos = UploadSet('photos', IMAGES)
-
-def init_flask_uploads(app: Flask):
-    configure_uploads(app, photos)
-    patch_request_class(app) 
+csrf = CSRFProtect()
